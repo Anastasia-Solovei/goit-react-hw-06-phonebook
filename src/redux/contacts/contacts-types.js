8a2f04752a7e9bf5bb7da5aea6e2bcb1be66a0ad
Anastasia Solovei <av.ignatyuk@gmail.com>
2021-09-13 23:05:09 +0300

    deletes file with the action types

--- src/redux/contacts/contacts-types.js
+++
@@ -1,5 +0,0 @@
-const ADD_CONTACT = "contactList/addContact";
-const DELETE_CONTACT = "contactList/deleteContact";
-const CHANGE_FILTER = "contactList/changeFilter";
-
-export default { ADD_CONTACT, DELETE_CONTACT, CHANGE_FILTER };
