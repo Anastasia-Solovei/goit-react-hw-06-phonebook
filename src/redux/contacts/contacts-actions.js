7b0b751117a428bfaa7906d588e08e6b2f0d603f
Anastasia Solovei <av.ignatyuk@gmail.com>
2021-09-14 02:04:37 +0300

    comments checkUniqueContact action

--- src/redux/contacts/contacts-actions.js
+++ src/redux/contacts/contacts-actions.js
@@ -4,6 +4,6 @@ import { createAction } from "@reduxjs/toolkit";
 export const addContact = createAction("contactList/addContact");
 export const deleteContact = createAction("contactList/deleteContact");
 export const changeFilter = createAction("contactList/changeFilter");
-export const checkUniqueContact = createAction(
-  "contactList/checkUniqueContact"
-);
+// export const checkUniqueContact = createAction(
+//   "contactList/checkUniqueContact"
+// );
