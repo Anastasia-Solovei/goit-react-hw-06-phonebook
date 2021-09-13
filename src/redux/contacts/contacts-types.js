518cbb8c2950ea2b0ec978a6423da1b9b032bc07
Anastasia Solovei <av.ignatyuk@gmail.com>
2021-09-13 19:42:17 +0300

    takes action types into a separate constants file

---
+++ src/redux/contacts/contacts-types.js
@@ -0,0 +1,5 @@
+const ADD_CONTACT = "contactList/addContact";
+const DELETE_CONTACT = "contactList/deleteContact";
+const FILTER_CONTACTS = "contactList/filterContacts";
+
+export default { ADD_CONTACT, DELETE_CONTACT, FILTER_CONTACTS };
