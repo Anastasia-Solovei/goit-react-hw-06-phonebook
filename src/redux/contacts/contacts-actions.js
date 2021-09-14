13389241f2f772729870be465559f092777e9821
Anastasia Solovei <av.ignatyuk@gmail.com>
2021-09-14 16:44:44 +0300

    deletes checkUniqueContact from actions

--- src/redux/contacts/contacts-actions.js
+++ src/redux/contacts/contacts-actions.js
@@ -1,9 +1,5 @@
-// import actionTypes from "../contacts/contacts-types";
 import { createAction } from "@reduxjs/toolkit";
 
 export const addContact = createAction("contactList/addContact");
 export const deleteContact = createAction("contactList/deleteContact");
 export const changeFilter = createAction("contactList/changeFilter");
-// export const checkUniqueContact = createAction(
-//   "contactList/checkUniqueContact"
-// );
