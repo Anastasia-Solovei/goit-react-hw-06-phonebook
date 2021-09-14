3e97cf861d5e9252b32cdc571c279d7c86af0c43
Anastasia Solovei <av.ignatyuk@gmail.com>
2021-09-14 16:44:19 +0300

    deletes checkUniqueContact from reducer

--- src/redux/contacts/contacts-reducer.js
+++ src/redux/contacts/contacts-reducer.js
@@ -1,21 +1,17 @@
-import { combineReducers } from "redux";
-import { createReducer } from "@reduxjs/toolkit";
-import {
-  addContact,
-  deleteContact,
-  changeFilter,
-  checkUniqueContact,
-} from "./contacts-actions";
+import { createReducer, combineReducers } from "@reduxjs/toolkit";
+import { addContact, deleteContact, changeFilter } from "./contacts-actions";
 
-const itemsReducer = createReducer([], {
+const initialState = [
+  { id: "id-1", name: "Inna Palchynska", number: "459-12-56" },
+  { id: "id-2", name: "Halyna Sinko", number: "443-89-12" },
+  { id: "id-3", name: "Dariia Nikolaieva", number: "645-17-79" },
+  { id: "id-4", name: "Sergiy Prodan", number: "227-91-26" },
+];
+
+const itemsReducer = createReducer(initialState, {
   [addContact]: (state, { payload }) => [...state, payload],
   [deleteContact]: (state, { payload }) =>
     state.filter(({ id }) => id !== payload),
-  // [checkUniqueContact]: (state, { payload }) => {
-  //   const isExistContact = !!state.find((item) => item.name === payload);
-  //   isExistContact && alert("Contact is already exist!");
-  //   return !isExistContact;
-  // },
 });
 
 const filterReducer = createReducer("", {
